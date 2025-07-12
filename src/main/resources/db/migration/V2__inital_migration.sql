e99aab0223c58fa7648964189c58ef35feeaa712
Vikas Maury <mauryvikas12345@gmail.com>
2025-07-13 02:34:00 +0530

    first commit

---
+++ src/main/resources/db/migration/V2__inital_migration.sql
@@ -0,0 +1,23 @@
+-- store.addresses definition
+
+CREATE TABLE store.`users` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE store.`addresses` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `street` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `zip` varchar(255) NOT NULL,
+  `user_id` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `addresses_users_FK` (`user_id`),
+  CONSTRAINT `addresses_users_FK` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- store.users definition
