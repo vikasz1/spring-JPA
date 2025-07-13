63fd84a82a380ec760694cc04cc8b8b744c1d3e3
Vikas Maury <mauryvikas12345@gmail.com>
2025-07-13 11:28:21 +0530

    defining entities

---
+++ src/main/java/com/example/spring_JPA/entities/User.java
@@ -0,0 +1,50 @@
+package com.example.spring_JPA.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String username;
+    @Column(name="email")
+    private String email;
+    @Column(name = "password")
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
